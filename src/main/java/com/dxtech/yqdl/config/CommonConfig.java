03b96eb20250015ffabbaeca2197fa409df7a8e3
wangguoqiang <Wgq1099340878>
2021-08-02 09:39:54 +0800

    生产坏境ip

--- src/main/java/com/dxtech/yqdl/config/CommonConfig.java
+++ src/main/java/com/dxtech/yqdl/config/CommonConfig.java
@@ -3,5 +3,5 @@ package com.dxtech.yqdl.config;
 public class CommonConfig {
     public static String uploadFilePath = "C:/yqdl-upload/image/";
     public static String qrcodeFilePath = "C:/yqdl-upload/qrcode/";
-    public static String urlFormatPath = "http://192.168.0.102:8080/image/";
+    public static String urlFormatPath = "http://192.168.2.154:8080/image/";
 }
